47748365930abdc80b9d02a228ae7560b346806e
melvinswift <greatwinblowaway@gmail.com>
2022-12-04 17:29:05 +0700

    refactor

--- code/q1-1.py
+++ code/q1-1.py
@@ -1,17 +1,18 @@
-file = open("./data/q1", "r")
+file = open("../data/q1", "r")
 
 current = 0
-data = []
+max = 0
 
 for c in file.readlines():
     text = c.strip()
 
     if not text:
-        data.append(current)
+        if current > max:
+            max = current
         current = 0
     else:
         current += int(text)
 
-print("TOTAL TOP 3:", sum(sorted(data, reverse=True)[:3]))
+print("MAX:", max)
 
 file.close()
--- code/q1-2.py
+++ code/q1-2.py
@@ -1,18 +1,17 @@
-file = open("./data/q1", "r")
+file = open("../data/q1", "r")
 
 current = 0
-max = 0
+data = []
 
 for c in file.readlines():
     text = c.strip()
 
     if not text:
-        if current > max:
-            max = current
+        data.append(current)
         current = 0
     else:
         current += int(text)
 
-print("MAX:", max)
+print("TOTAL TOP 3:", sum(sorted(data, reverse=True)[:3]))
 
 file.close()
