6d0ba80877d43c3d86d7fa47cf743a0f5a0eb8c6
melvinswift <greatwinblowaway@gmail.com>
2022-12-04 17:28:16 +0700

    q1

---
+++ q1-1.py
@@ -0,0 +1,18 @@
+file = open("./data/q1", "r")
+
+current = 0
+max = 0
+
+for c in file.readlines():
+    text = c.strip()
+
+    if not text:
+        if current > max:
+            max = current
+        current = 0
+    else:
+        current += int(text)
+
+print("MAX:", max)
+
+file.close()
---
+++ q1-2.py
@@ -0,0 +1,17 @@
+file = open("./data/q1", "r")
+
+current = 0
+data = []
+
+for c in file.readlines():
+    text = c.strip()
+
+    if not text:
+        data.append(current)
+        current = 0
+    else:
+        current += int(text)
+
+print("TOTAL TOP 3:", sum(sorted(data, reverse=True)[:3]))
+
+file.close()
