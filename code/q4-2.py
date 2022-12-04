41c092049d5d4e02534373f2058ee2647e031ed1
melvinswift <greatwinblowaway@gmail.com>
2022-12-04 18:00:36 +0700

    q4

---
+++ code/q4-2.py
@@ -0,0 +1,18 @@
+file = open("../data/q4", "r")
+
+
+count = 0
+
+for line in file.readlines():
+    work1, work2 = line.strip().split(",")
+
+    start1, stop1 = [int(e) for e in work1.strip().split("-")]
+    start2, stop2 = [int(e) for e in work2.strip().split("-")]
+
+    if (start1 >= start2 and start1 <= stop2) or (start2 >= start1 and start2 <= stop1):
+        count += 1
+
+
+print("Count:", count)
+
+file.close()
