d6b939a6796483ef30d6676fc1774fe1c2e61840
melvinswift <greatwinblowaway@gmail.com>
2022-12-07 11:47:44 +0700

    q6

---
+++ code/q6-1.py
@@ -0,0 +1,28 @@
+file = open("../data/q6", "r")
+
+
+def is_all_unique(word):
+    word_set = set([c for c in word])
+    if len(word) != len(word_set):
+        return False
+    else:
+        return True
+
+
+data = file.readline().strip()
+
+cache_data = ""
+
+for i in range(len(data)):
+    cache_data += data[i]
+
+    if len(cache_data) < 4:
+        continue
+
+    if is_all_unique(cache_data[-4:]):
+        break
+
+
+print("result:", len(cache_data))
+
+file.close()
